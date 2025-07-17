979ef5bf630dee28fac213315bfab9f97d8bf844
minorselvam <galaxy.mskumar@gmail.com>
2025-07-17 18:44:47 +0530

    Code updated to read the azure sql db connection string from the key vault.

---
+++ ProductMicroservice/ProductMicroservice/AzureConnector/SqlConnectionProvider.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace ProductMicroservice.AzureConnector
+{
+    public class SqlConnectionProvider
+    {
+        private readonly string _vaultUrl = "https://testkeyvaultmsk.vault.azure.net/";
+        private readonly string _secretName = "DbConnectionString";
+
+        public async Task<string> GetConnectionStringAsync()
+        {
+            var client = new SecretClient(new Uri(_vaultUrl), new DefaultAzureCredential());
+            KeyVaultSecret secret = await client.GetSecretAsync(_secretName);
+            return secret.Value;
+        }
+
+    }
+}
