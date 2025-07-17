44b44ec0f7259ccf38efbc537f75fc4cb3eafba9
minorselvam <galaxy.mskumar@gmail.com>
2025-07-17 10:43:28 +0530

    initial commit

---
+++ ProductMicroservice/ProductMicroservice/DBContext/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Models;
+
+public class ProductContext : DbContext
+{
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().ToTable("Product");
+    }
+}
